972edca64e4871a9a6b2c5baaadcadb6ccc8b0aa
zchen8 <zchen8@paypal.com>
2015-10-04 13:28:23 -0700

    admin

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,45 +1,41 @@
 $(document).ready(function(){
-
     $("#submitKids").submit(function(e){
-
+        //console.log( "submit :", $( this ).serializeArray() );
         e.preventDefault();
-
+        console.log("rakesh");
         var name = $("#name").val();
         console.log("name :", name);
         var region = $("#region").val();
         var note = $("#biography").val();
         var donor = $("#donor").val();
-
-       var submitData = {
-           "_id": name,
-           "name": name,
-           "region": region,
-           "donors": [
-               {
-                   "donor": donor
-               }
-           ],
-           "timeline": [
-               {
-                   "picture": "/image/"+ name + "png",
-                   "date": "09-01-2015",
-                   "note": "graduation"
-               }
-           ]
-       };
-
-        console.log(submitData);
-
+        var picDate = $("#picDate").val();
+        var picNote = $("#picNote").val();
+        //var data = $( this ).serializeArray();
 
         $.ajax({
-            data: submitData,
+            data: {
+                "_id": name,
+                "name": name,
+                "region": region,
+                "donors": [
+                    {
+                        "donor": donor
+                    }
+                ],
+                "timeline": [
+                    {
+                        "picture": "pic1",
+                        "date": picDate,
+                        "note": picNote
+                    }
+                ]
+            },
             url: 'http://localhost:3000/insertKids',
             type: 'POST',
             dataType: 'json',
             //jsonpCallback: 'callback', // this is not relevant to the POST anymore
             success: function (data) {
                 console.log('process sucess : ', data);
-                $('#connectKids').modal('hide');
             }
 
         });
