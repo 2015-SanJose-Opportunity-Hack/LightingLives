3dc87617ee1fea66689c0015faeaec1ee7b70750
Rakesh Soni <rsoni@paypal.com>
2015-10-04 12:51:15 -0700

    adding links and nodes

--- routes/routes.js
+++ routes/routes.js
@@ -54,19 +54,56 @@ exports.insertKids = function(req, res) {
         console.log("data insert error : ", error);
        res.redirect('/');
     });
+    console.log('insertKids : done');
    if(req.body.donors.length > 0){
     var donor = req.body.donors[0].donor;
-    console.log("updating donor :", donor);   
     var cursor =db.collection('donors').find();
     cursor.each(function(err, doc) {
         if (doc != null) {
             console.log("list of nodes: ", JSON.stringify(doc));
-            //var nodes = 
+            var nodes = doc["nodes"];
+            var nodeSize = nodes.length;
+            var links = doc["links"];
+            var linksSize = links.length;
+
+            console.log("nodes : ", JSON.stringify(nodes));
+            console.log("links : ", JSON.stringify(links));
+            console.log("nodes size :", nodeSize);
+            console.log("links size :", linksSize);
+
+            var regionIndex = 0;
+
+            for(var i = 0 ; i < nodes.length; i++){
+                if(nodes[i].name == req.body.region){
+                    regionIndex = nodes[i].label;
+                }
+            }
+
+            var kidIndex = nodeSize + 1;
+
             //res.writeHead(200, { "Content-Type": "application/json" });
             //var response = {'x':'2'};
-            //res.end(JSON.stringify(doc));
+            console.log("updating donor :", donor);   
+            db.collection('donors').update({}, {
+                $addToSet:{
+                    nodes : {
+                        "name": req.body.name,
+                        "label": kidIndex,
+                        "image": req.body.timeline[0].picture,
+                        "group": 7
+                    },
+                    links : {
+                        "source": regionIndex,
+                        "target": kidIndex,
+                        "value": 1
+                      }
+                }
+            });
+            res.end(JSON.stringify(doc));
+
         }
-}); 
+    });
+ 
    }
 //
 
